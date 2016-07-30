b144a7b571e88ee78ea676c6cc6b7fd4873afd9e
Matthijs Alles <admin@bixie.nl>
2016-07-30 14:32:29 +0200

    Add FluentSetterAssembler

---
+++ src/Phpro/SoapClient/CodeGenerator/Assembler/FluentSetterAssembler.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Phpro\SoapClient\CodeGenerator\Assembler;
+
+use Phpro\SoapClient\CodeGenerator\Assembler\AssemblerInterface;
+use Phpro\SoapClient\CodeGenerator\Context\ContextInterface;
+use Phpro\SoapClient\CodeGenerator\Context\PropertyContext;
+use Phpro\SoapClient\CodeGenerator\Util\Normalizer;
+use Phpro\SoapClient\Exception\AssemblerException;
+use Zend\Code\Generator\DocBlockGenerator;
+use Zend\Code\Generator\MethodGenerator;
+
+/**
+ * Class SetterAssembler
+ *
+ * @package Phpro\SoapClient\CodeGenerator\Assembler
+ */
+class FluentSetterAssembler implements AssemblerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function canAssemble(ContextInterface $context)
+    {
+        return $context instanceof PropertyContext;
+    }
+
+    /**
+     * @param ContextInterface|PropertyContext $context
+     *
+     * @throws AssemblerException
+     */
+	public function assemble(ContextInterface $context)
+	{
+		$class = $context->getClass();
+		$property = $context->getProperty();
+		try {
+			$methodName = Normalizer::generatePropertyMethod('set', $property->getName());
+			$class->removeMethod($methodName);
+			$class->addMethodFromGenerator(
+				MethodGenerator::fromArray([
+					'name' => $methodName,
+					'parameters' => [$property->getName()],
+					'visibility' => MethodGenerator::VISIBILITY_PUBLIC,
+					'body' => sprintf('$this->%1$s = $%1$s;%2$sreturn $this;', $property->getName(), "\n"),
+					'docblock' => DocBlockGenerator::fromArray([
+						'tags' => [
+							[
+								'name' => 'param',
+								'description' => sprintf('%s $%s', $property->getType(), $property->getName())
+							],
+							[
+								'name' => 'return',
+								'description' => '$this'
+							]
+						]
+					])
+				])
+			);
+		} catch (\Exception $e) {
+			throw AssemblerException::fromException($e);
+		}
+	}
+}
