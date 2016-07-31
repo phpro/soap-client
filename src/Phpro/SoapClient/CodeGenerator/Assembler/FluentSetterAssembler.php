32051dcb06870751b79e8da2f74c70a17c49a2be
Matthijs Alles <admin@bixie.nl>
2016-08-01 01:24:20 +0200

    fix indentation

--- src/Phpro/SoapClient/CodeGenerator/Assembler/FluentSetterAssembler.php
+++ src/Phpro/SoapClient/CodeGenerator/Assembler/FluentSetterAssembler.php
@@ -20,45 +20,42 @@ class FluentSetterAssembler implements AssemblerInterface
     /**
      * {@inheritdoc}
      */
-    public function canAssemble(ContextInterface $context)
-    {
+    public function canAssemble (ContextInterface $context) {
         return $context instanceof PropertyContext;
     }
 
     /**
      * @param ContextInterface|PropertyContext $context
-     *
      * @throws AssemblerException
      */
-	public function assemble(ContextInterface $context)
-	{
-		$class = $context->getClass();
-		$property = $context->getProperty();
-		try {
-			$methodName = Normalizer::generatePropertyMethod('set', $property->getName());
-			$class->removeMethod($methodName);
-			$class->addMethodFromGenerator(
-				MethodGenerator::fromArray([
-					'name' => $methodName,
-					'parameters' => [$property->getName()],
-					'visibility' => MethodGenerator::VISIBILITY_PUBLIC,
-					'body' => sprintf('$this->%1$s = $%1$s;%2$sreturn $this;', $property->getName(), "\n"),
-					'docblock' => DocBlockGenerator::fromArray([
-						'tags' => [
-							[
-								'name' => 'param',
-								'description' => sprintf('%s $%s', $property->getType(), $property->getName())
-							],
-							[
-								'name' => 'return',
-								'description' => '$this'
-							]
-						]
-					])
-				])
-			);
-		} catch (\Exception $e) {
-			throw AssemblerException::fromException($e);
-		}
-	}
+    public function assemble (ContextInterface $context) {
+        $class = $context->getClass();
+        $property = $context->getProperty();
+        try {
+            $methodName = Normalizer::generatePropertyMethod('set', $property->getName());
+            $class->removeMethod($methodName);
+            $class->addMethodFromGenerator(
+                MethodGenerator::fromArray([
+                    'name' => $methodName,
+                    'parameters' => [$property->getName()],
+                    'visibility' => MethodGenerator::VISIBILITY_PUBLIC,
+                    'body' => sprintf('$this->%1$s = $%1$s;%2$sreturn $this;', $property->getName(), "\n"),
+                    'docblock' => DocBlockGenerator::fromArray([
+                        'tags' => [
+                            [
+                                'name' => 'param',
+                                'description' => sprintf('%s $%s', $property->getType(), $property->getName())
+                            ],
+                            [
+                                'name' => 'return',
+                                'description' => '$this'
+                            ]
+                        ]
+                    ])
+                ])
+            );
+        } catch (\Exception $e) {
+            throw AssemblerException::fromException($e);
+        }
+    }
 }
