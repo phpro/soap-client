1fe9c81d308d32a70e1e5aa1d3d58a94d9c64e8c
Matthijs Alles <admin@bixie.nl>
2016-08-01 01:18:54 +0200

    fix indentation

--- src/Phpro/SoapClient/CodeGenerator/Assembler/TraitAssembler.php
+++ src/Phpro/SoapClient/CodeGenerator/Assembler/TraitAssembler.php
@@ -17,52 +17,48 @@ class TraitAssembler implements AssemblerInterface
      * @var
      */
     private $traitName;
-	/**
-	 * @var
-	 */
-	private $traitAlias;
+    /**
+     * @var
+     */
+    private $traitAlias;
 
-	/**
-	 * TraitAssembler constructor.
-	 * @param $traitName
-	 * @param $traitAlias
-	 */
-    public function __construct($traitName, $traitAlias = null)
-    {
+    /**
+     * TraitAssembler constructor.
+     * @param $traitName
+     * @param $traitAlias
+     */
+    public function __construct ($traitName, $traitAlias = null) {
         $this->traitName = $traitName;
-		$this->traitAlias = $traitAlias;
-	}
+        $this->traitAlias = $traitAlias;
+    }
 
     /**
      * @param ContextInterface $context
-     *
      * @return bool
      */
-    public function canAssemble(ContextInterface $context)
-    {
+    public function canAssemble (ContextInterface $context) {
         return $context instanceof TypeContext;
     }
 
     /**
      * @param ContextInterface|TypeContext $context
      */
-    public function assemble(ContextInterface $context)
-    {
+    public function assemble (ContextInterface $context) {
         $class = $context->getClass();
 
         try {
             if (!in_array($this->traitName, $class->getUses())) {
                 $class->addUse($this->traitName, $this->traitAlias);
             }
-			$traitAlias = $this->traitAlias;
-			if (!$traitAlias) {
-				$a = explode('\\', $this->traitName);
-				$traitAlias = array_pop($a);
-			}
-			$traits = $class->getTraits();
-			if (!in_array($traitAlias, $traits)) {
-				$class->addTrait($traitAlias);
-			}
+            $traitAlias = $this->traitAlias;
+            if (!$traitAlias) {
+                $a = explode('\\', $this->traitName);
+                $traitAlias = array_pop($a);
+            }
+            $traits = $class->getTraits();
+            if (!in_array($traitAlias, $traits)) {
+                $class->addTrait($traitAlias);
+            }
         } catch (\Exception $e) {
             throw AssemblerException::fromException($e);
         }
