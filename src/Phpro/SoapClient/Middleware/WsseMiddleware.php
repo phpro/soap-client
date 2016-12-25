78037dbd0e957949ffb827eebeb9d6d31e150bc5
Toon Verwerft <toonverwerft@gmail.com>
2016-12-25 11:58:50 +0100

    First attempt to WSSE - TODO: split up into a pipeline of actions

--- src/Phpro/SoapClient/Middleware/WsseMiddleware.php
+++ src/Phpro/SoapClient/Middleware/WsseMiddleware.php
@@ -6,6 +6,7 @@ use Phpro\SoapClient\Xml\SoapXml;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use RobRichards\WsePhp\WSSESoap;
+use RobRichards\XMLSecLibs\XMLSecurityKey;
 
 /**
  * Class WsseMiddleware
@@ -17,20 +18,23 @@ class WsseMiddleware extends Middleware
     /**
      * @var callable
      */
-    private $wssePrepare;
+    private $prepareWsseRequest;
 
     /**
      * @var callable
      */
-    private $wsseResolve;
+    private $prepareWsseResponse;
 
     /**
      * WsseMiddleware constructor.
+     *
+     * @param callable $prepareWsseRequest
+     * @param callable $prepareWsseResponse
      */
-    public function __construct(callable $wssePrepare, callable $wsseResolve)
+    public function __construct(callable $prepareWsseRequest, callable $prepareWsseResponse)
     {
-        $this->wssePrepare = $wssePrepare;
-        $this->wsseResolve = $wsseResolve;
+        $this->prepareWsseRequest = $prepareWsseRequest;
+        $this->prepareWsseResponse = $prepareWsseResponse;
     }
 
     /**
@@ -43,7 +47,7 @@ class WsseMiddleware extends Middleware
         $xml = SoapXml::fromStream($request->getBody()->getContents());
 
         $wsse = new WSSESoap($xml->getXmlDocument());
-        $this->wssePrepare($wsse);
+        $this->prepareWsseRequest($wsse, $xml);
 
         $request = $request->withBody($xml->toStream());
 
@@ -58,8 +62,120 @@ class WsseMiddleware extends Middleware
         $xml = SoapXml::fromStream($response->getBody()->getContents());
 
         $wsse = new WSSESoap($xml->getXmlDocument());
-        $this->wsseResolve($wsse);
+        $this->prepareWsseResponse($wsse, $xml);
 
         return $response->withBody($xml->toStream());
+    }
+
+    /**
+     * @return WsseMiddleware
+     */
+    public static function wsa(
+        string $publicKey,
+        string $privateKey,
+        string $dsigMethod = XMLSecurityKey::RSA_SHA1
+    ): WsseMiddleware
+    {
+        return new self(
+            function(WSSESoap $wsse) use ($publicKey, $privateKey, $dsigMethod) {
+                // Make sure the WSA headers get signed and add a timestamp:
+                $wsse->signAllHeaders = true;
+                $wsse->addTimestamp();
+
+                // Sign the SOAP document with your private key:
+                $key = new XMLSecurityKey($dsigMethod, ['type' => 'private']);
+                $key->loadKey($privateKey, true);
+                $wsse->signSoapDoc($key);
+
+                // Add the public key to the request:
+                $token = $wsse->addBinaryToken(file_get_contents($publicKey));
+                $wsse->attachTokentoSig($token);
+            },
+            function() {}
+        );
+    }
+
+    /**
+     * TODO: certificates
+     *
+     * @param string $username
+     * @param string $password
+     * @param bool   $digest
+     *
+     * @return WsseMiddleware
+     */
+    public static function userSigned(string $username, string $password, bool $digest = false)
+    {
+        return new self(
+            function (WSSESoap $wsse) use ($username, $password, $digest) {
+                /* Sign all headers to include signing the WS-Addressing headers */
+                $wsse->signAllHeaders = true;
+                $wsse->addTimestamp();
+                $wsse->addUserToken($username, $password, $digest);
+
+                /* create new XMLSec Key using RSA SHA-1 and type is private key */
+                $key = new XMLSecurityKey(XMLSecurityKey::RSA_SHA1, array('type' => 'private'));
+                $key->loadKey(PRIVATE_KEY, true);
+                $wsse->signSoapDoc($key);
+
+                /* Add certificate (BinarySecurityToken) to the message and attach pointer to Signature */
+                $token = $wsse->addBinaryToken(file_get_contents(CERT_FILE));
+                $wsse->attachTokentoSig($token);
+            },
+            function() {}
+        );
+        
+    }
+
+    /**
+     * @return WsseMiddleware
+     * @throws \Exception
+     */
+    public static function encrypt(
+        string $publicKey,
+        string $privateKey,
+        string $serviceCertificate,
+        string $dsigMethod = XMLSecurityKey::RSA_SHA1
+    )
+    {
+        return new self(
+            function(WSSESoap $wsse) use ($publicKey, $privateKey, $serviceCertificate, $dsigMethod) {
+                $wsse->addTimestamp();
+
+                $key = new XMLSecurityKey($dsigMethod, ['type' => 'private']);
+                $key->loadKey(PRIVATE_KEY, true);
+
+                /* Sign the message - also signs appropiate WS-Security items */
+                $wsse->signSoapDoc($key, [
+                    'insertBefore' => false
+                ]);
+
+                /* Add certificate (BinarySecurityToken) to the message */
+                $token = $wsse->addBinaryToken(file_get_contents($publicKey));
+                $wsse->attachTokentoSig($token);
+
+                /* Attach pointer to Signature */
+                $key = new XMLSecurityKey(XMLSecurityKey::AES256_CBC);
+                $key->generateSessionKey();
+                $siteKey = new XMLSecurityKey(XMLSecurityKey::RSA_OAEP_MGF1P, ['type' => 'public']);
+                $siteKey->loadKey($serviceCertificate, true, true);
+                $wsse->encryptSoapDoc($siteKey, $key,  [
+                    'KeyInfo' => [
+                        'X509SubjectKeyIdentifier' => true
+                    ]
+                ]);
+            },
+            function(WSSESoap $wsse, SoapXml $xml) use($privateKey) {
+                $wsse->decryptSoapDoc($xml->getXmlDocument(), [
+                    'keys' => [
+                        'private' => [
+                            'key' => $privateKey,
+                            'isFile' => true,
+                            'isCert' => false
+                        ]
+                    ]
+                ]);
+            }
+        );
     }
 }
