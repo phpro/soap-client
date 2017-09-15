f25fabab54c973103d4f21c589f93a9edfba250d
Marek Pietrzak <marekpietrzak@gmail.com>
2017-09-15 15:39:46 +0100

    Add ExtendAssembler

---
+++ src/Phpro/SoapClient/CodeGenerator/Assembler/ExtendAssembler.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Phpro\SoapClient\CodeGenerator\Assembler;
+
+use Phpro\SoapClient\CodeGenerator\Context\ContextInterface;
+use Phpro\SoapClient\CodeGenerator\Context\TypeContext;
+use Phpro\SoapClient\Exception\AssemblerException;
+use Zend\Code\Generator\ClassGenerator;
+
+/**
+ * Class ExtendAssembler
+ *
+ * @package Phpro\SoapClient\CodeGenerator\Assembler
+ */
+class ExtendAssembler implements AssemblerInterface
+{
+    /**
+     * @var string
+     */
+    private $extendedClassName;
+
+    /**
+     * ExtendedAssembler constructor.
+     *
+     * @param $extendedClassName
+     */
+    public function __construct($extendedClassName)
+    {
+        $this->extendedClassName = $extendedClassName;
+    }
+
+    /**
+     * @param ContextInterface $context
+     *
+     * @return bool
+     */
+    public function canAssemble(ContextInterface $context)
+    {
+        return $context instanceof TypeContext;
+    }
+
+    /**
+     * @param ContextInterface|TypeContext $context
+     */
+    public function assemble(ContextInterface $context)
+    {
+        $class = $context->getClass();
+        $extendedClassName = $this->extendedClassName;
+
+        if ($this->isExtendingItself($class)) {
+            return;
+        }
+
+        try {
+            $useAssembler = new UseAssembler($extendedClassName);
+            if ($useAssembler->canAssemble($context)) {
+                $useAssembler->assemble($context);
+            }
+
+            $class->setExtendedClass($extendedClassName);
+
+        } catch (\Exception $e) {
+            throw AssemblerException::fromException($e);
+        }
+    }
+
+    /**
+     * @param ClassGenerator $class
+     * @return bool
+     */
+    private function isExtendingItself(ClassGenerator $class)
+    {
+        $fullClassName = $class->getNamespaceName() . '\\' . $class->getName();
+
+        return $this->extendedClassName === $fullClassName;
+    }
+}
