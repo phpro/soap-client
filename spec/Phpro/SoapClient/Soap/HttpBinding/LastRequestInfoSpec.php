454b23a50ce204c9484b32e67dd9ae59f8a25326
Ezekiel Motlana <ezekielm@miway.co.za>
2017-10-09 16:50:06 +0200

    Added a copyright disclaimer on all the test files

--- spec/Phpro/SoapClient/Soap/HttpBinding/LastRequestInfoSpec.php
+++ spec/Phpro/SoapClient/Soap/HttpBinding/LastRequestInfoSpec.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Phpro application.
+ *
+ * Copyright (c) 2015-2017 Phpro
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace spec\Phpro\SoapClient\Soap\HttpBinding;
 
 use Phpro\SoapClient\Soap\SoapClient;
