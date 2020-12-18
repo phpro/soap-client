7ca695c70bc0943cc5892920fecaf9e509abebc1
Toon Verwerft <toonverwerft@gmail.com>
2020-12-18 10:23:25 +0100

    Fix breaking laminas code change (#335)

--- src/Phpro/SoapClient/CodeGenerator/ClientGenerator.php
+++ src/Phpro/SoapClient/CodeGenerator/ClientGenerator.php
@@ -43,7 +43,8 @@ class ClientGenerator implements GeneratorInterface
     public function generate(FileGenerator $file, $client): string
     {
         try {
-            $class = $file->getClass();
+            // @phpstan-ignore-next-line
+            $class = $file->getClass() ?: new ClassGenerator();
         } catch (ClassNotFoundException $exception) {
             $class = new ClassGenerator();
         }
