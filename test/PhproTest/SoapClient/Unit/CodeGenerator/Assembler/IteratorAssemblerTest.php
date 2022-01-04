a9e05d1c877e01aed6182a3397a15a03bf5ef4bf
Simon Sippert <s.sippert@techdivision.com>
2022-01-04 17:47:03 +0100

    Update test

--- test/PhproTest/SoapClient/Unit/CodeGenerator/Assembler/IteratorAssemblerTest.php
+++ test/PhproTest/SoapClient/Unit/CodeGenerator/Assembler/IteratorAssemblerTest.php
@@ -60,7 +60,7 @@ class MyType implements IteratorAggregate
 {
 
     /**
-     * @return \ArrayIterator
+     * @return \ArrayIterator|array[]
      * @phpstan-return \ArrayIterator<array-key, array>
      * @psalm-return \ArrayIterator<array-key, array>
      */
