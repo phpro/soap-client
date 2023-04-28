98aeca75594b55e124e682300a279efc1cb438cd
Toon Verwerft <toonverwerft@gmail.com>
2023-04-28 13:27:57 +0200

    Remove PHP <8 code

--- test/PhproTest/SoapClient/Unit/CodeGenerator/Model/PropertyTest.php
+++ test/PhproTest/SoapClient/Unit/CodeGenerator/Model/PropertyTest.php
@@ -17,24 +17,8 @@ class PropertyTest extends TestCase
     /**
      * @test
      */
-    public function it_returns_null_type_pre_php8(): void
-    {
-        if (PHP_VERSION_ID >= 80000) {
-            self::markTestSkipped('Pre PHP 8 only');
-        }
-        $property = new Property('test', 'mixed', 'App', new TypeMeta());
-        self::assertNull($property->getPhpType());
-        self::assertEquals('mixed', $property->getType());
-    }
-
-    /**
-     * @test
-     */
     public function it_returns_mixed_type_post_php8(): void
     {
-        if (PHP_VERSION_ID < 80000) {
-            self::markTestSkipped('Post PHP 8 only');
-        }
         $property = new Property('test', 'mixed', 'App', new TypeMeta());
         self::assertEquals('mixed', $property->getPhpType());
         self::assertEquals('mixed', $property->getType());
